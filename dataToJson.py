29b8c2d33fce763b9d7f7a8014e8920ada55a319
Melvin Campos Casares <melvin.campos@outlook.be>
2020-11-08 23:18:17 +0100

    Data to JSON converter now accept modified path flag when called
    usage: dataToJson.py [-h] [-f FILE] [-v]
    
    optional arguments:
      -h, --help            show this help message and exit
      -f FILE, --file FILE  Path of the original file to convert
      -v, --version         dataToJson.py program version

--- dataToJson.py
+++ dataToJson.py
@@ -13,15 +13,18 @@ data_path = "data/Test Your Awareness eyes data.txt";
 JSON_FILE = "PoR-data.json";
 
 ''' arguments available to launch the app in a specific way '''
-feature = argparse.ArgumentParser(prog='Data to JSON converter', add_help=True, prefix_chars='-', formatter_class=argparse.RawTextHelpFormatter, description=textwrap.dedent('''\
+feature = argparse.ArgumentParser(prog='dataToJson.py', add_help=True, prefix_chars='-', formatter_class=argparse.RawTextHelpFormatter, description=textwrap.dedent('''\
         Point of Regard - Data to JSON converter module
         -----------------------------------------------
         This script is meant to be used in order to convert data to a suitable format, JSON.
         It takes necessary data from the original file and write a new one.
         '''))
-#feature.add_argument('-f', '--file', help='Path of the original file to convert', type=str, default=data_path, required=False)
+feature.add_argument('-f', '--file', help='Path of the original file to convert', type=str, default=data_path, required=False)
 feature.add_argument('-v', '--version', help='%(prog)s program version', action='version', version='%(prog)s v0.1')
 args = feature.parse_args()
+
+if args.file:
+    data_path = args.file
 
 ''' Log configuration '''
 logger = logging.getLogger('PoR-data')
@@ -46,29 +49,41 @@ def fail(msg):
 
 def converter(src, dst):
     logger.info('Source file path: %s', src)
-    logger.info('Exportation file name: %s', dst)
+    if args.file:
+        logger.info('Exportation file name: %s', args.file)
+    else:
+        logger.info('Exportation file name: %s', dst)
     data = []
-    with open(src, 'r') as f:
-        #f.readlines()[2:]
-        # Read each line and trims of extra spaces and gives only the valid words
-        for line in f:
-            value = list(line.strip().split())
-            logger.debug(value)
-            # Creating dictionary for each parameter
-            temp_data = {
-                    'time': value[0],
-                    'type': value[1],
-                    'LporX': value[21],
-                    'LporY': value[22],
-                    'RporX': value[23],
-                    'RporY': value[24]
-            }
-            data.append(temp_data)
-    # Creating the JSON file
-    with open(dst, 'w') as out_file:
-        json.dump(data, out_file, indent = 2)
-    out_file.close()
-    logger.info('File has been successfully converted in JSON')
+    try:
+        with open(src, 'r') as f:
+            #f.readlines()[2:]
+            # Read each line and trims of extra spaces and gives only the valid words
+            for line in f:
+                value = list(line.strip().split())
+                logger.debug(value)
+                # Creating dictionary for each parameter
+                temp_data = {
+                        'time': value[0],
+                        'type': value[1],
+                        'LporX': value[21],
+                        'LporY': value[22],
+                        'RporX': value[23],
+                        'RporY': value[24]
+                }
+                data.append(temp_data)
+    except IOError as e:
+        fail('IOError while trying to open and read source file')
+        raise
+
+    try:
+        # Creating the JSON file
+        with open(dst, 'w') as out_file:
+            json.dump(data, out_file, indent = 2)
+        out_file.close()
+        logger.info('File has been successfully converted in JSON')
+    except IOError as e:
+        fail('IOError while trying to open and write destination file')
+        raise
 
 
 if __name__ == "__main__":
