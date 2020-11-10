d4b6b9a92cdf5db2f4cc345a7a81c6b7fdcdc65a
diomenios <loui.arys@gmail.com>
2020-11-10 21:22:23 +0100

    version de sauvegarde

--- read-video.py
+++ read-video.py
@@ -1,17 +1,91 @@
-import cv2
+#import cv2
+import json
+import time
 import numpy as np
+import cv2
 
+from matplotlib import pyplot as plt, image as mpimg
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
 
-# number of images per second
+# variable definition
 FRAME_RATE = 34
+PATH_TO_VIDEO = "data/test-your-awareness.avi"
+PATH_TO_JSON_FILE = "PoR-data.json"
 
 
-cap = cv2.VideoCapture("data/test-your-awareness.avi")
+def readDataFromJsonFile(path):
+    """
+    permet de lire les données venant d'un fichier JSON
+
+    :param path: chemin vers le fichier à lire
+    :return: un tableau d'objets JSON (iteratif)
+    """
+    with open(path) as json_file:
+        return json.load(json_file)
+
+
+def simpleConcatData(data, frameCount):
+    """
+    TODO : implémenter la partie du code faisant la moyenne des différents paquets
+
+    Permet de séparer les datas passées en paramètre en petits paquets. Le nombre de data par paquet
+    est déterminé par la taille des datas et le nombre de frames de la vidéo
+
+    :param data: tableau d'objets à trier
+    :param frameCount: le nombre de d'images d'une vidéo
+    :return: un tableau d'objets JSON (iteratif)(TODO)
+    """
+    dataNumber = len(data)
+    length = frameCount
+    packetSize = round(dataNumber / length)
+    cutFrequency = round(dataNumber / (packetSize * length - dataNumber))
+
+    frameIncr = 0
+    packetIncr = 0
+
+    return_data = []
+
+    for index, p in enumerate(data):
+
+        packetIncr += 1
+
+        if packetIncr == 1:
+            return_data[frameIncr] = p
+
+        else:
+
+            if (index+1) % cutFrequency == 0:
+
+                    packetIncr += 1
+
+            if packetIncr >= packetSize:
+
+                    packetIncr = 0
+                    frameIncr += 1
+
+    print("total" + str(frameIncr))
+
+
+data = readDataFromJsonFile(PATH_TO_JSON_FILE)
+
+cap = cv2.VideoCapture(PATH_TO_VIDEO)
+
+fig, ax = plt.subplots()
+
+img = mpimg.imread('moi.jpg')
+
+imgplot = plt.imshow(img)
+
+
+#simpleConcatData(data, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
 
 # Check if camera opened successfully
-if (cap.isOpened() == False):
+
+
+### Cette partie du code permet de lire la video avec CV2 ##############################
+
+"""if (cap.isOpened() == False):
     print("Error opening video stream or file")
 
 # Read until video is completed
@@ -34,4 +108,4 @@ while (cap.isOpened()):
 cap.release()
 
 # Closes all the frames
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()"""
