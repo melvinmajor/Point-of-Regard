827d64b159e93df7e8792bc72cab46fd2a250bc1
EXSamus <55688566+EXSamus@users.noreply.github.com>
2020-11-27 15:54:57 +0100

    change framerate

--- read-video.py
+++ read-video.py
@@ -1,11 +1,12 @@
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
 
 # number of images per second
-FRAME_RATE = 34
+FRAME_RATE = 25
 
 
 cap = cv2.VideoCapture("data/test-your-awareness.avi")
