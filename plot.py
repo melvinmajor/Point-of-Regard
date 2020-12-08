93049519ed3a9ca434feb5d16c356a07b2ddb24a
diomenios <loui.arys@gmail.com>
2020-12-08 23:24:14 +0100

    add for commit

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from scipy.interpolate import interp1d, UnivariateSpline
 
 PATH_TO_VIDEO = "data/test-your-awareness.avi"
-FRAME_RATE = 1000
+FRAME_RATE = 30
 FRAME_HIGH = 710
 FRAME_LENGTH = 950
 
@@ -67,7 +67,6 @@ def plotDotOnImage(image, dot):
     # PLOTTING : plot the dot to the x, y position
     ax.plot(dot['x'], dot['y'], 'bo')
 
-    # TODO : Optimize this part because it's cause fps drop
     canvas.draw()
     buf = canvas.buffer_rgba()
     # convert to a NumPy array
@@ -121,42 +120,93 @@ def readVideo(data):
 
     # Closes all the frames
     cv2.destroyAllWindows()
-    
+
+
+def customReadVideo(data):
+    cap = cv2.VideoCapture(PATH_TO_VIDEO)
+
+    if (cap.isOpened() == False):
+        print("Error opening video stream or file")
+
+    i = 0
+
+    # Read until video is completed
+    while (cap.isOpened()):
+
+        ret, frame = cap.read()
+
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        movingDot = {'x': data[i]["LporX"] / 1.6, 'y': data[i]["LporY"] / 1.6, 'positive_x': False, 'positive_y': False}
+
+        cv2.waitKey(int(1000/FRAME_RATE))
+
+        drawSquarre(30, int(data[i]["LporX"] / 1.6), int(data[i]["LporY"] / 1.6), frame)
+
+        cv2.imshow('frame', frame)
+
+        plt.close('all')
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        i += 1
+
+    # When everything done, release the video capture object
+    cap.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+
 """ *********************************** TODO functions ****************************************** """
 
-"""def drawCircle(radius, x_center, y_center, image, color):
+"""def drawCircle(radius, x_center, y_center, list, image, color):
 
-    squarre_radius = (radius-1)*(radius-1)
-    list = []
+    x=0,
+    y=radius
+    m=5-4*radius
 
-    for i in range((2*radius)+1):
+    while x <=y :
+        list[x_center+x][y_center+y] = "*"
+        list[x_center+y][y_center+x] = "*"
+        list[x_center-x][y_center+y] = "*"
+        list[x_center-y][y_center+x] = "*"
+        list[x_center+x][y_center+y] = "*"
+        list[x_center+y][y_center+x] = "*"
+        list[x_center-x][y_center-y] = "*"
+        list[x_center-y][y_center-x] = "*"
 
-        stock = []
-        for j in range((2*radius)+1):
-            stock.append("o")
-        list.append(stock)
-
-    for i in (0, radius):
-
-        x = 0
-        i_squarre = i * i
-        while (squarre_radius - i_squarre >= x * x):
-            list[radius + x][radius + i] = "*"
-            list[radius + x][radius - i] = "*"
-            list[radius + i][radius - x] = "*"
-            list[radius - i][radius - x] = "*"
-            x+=1
+        if m > 0 :
+            y -= 1
+            m = m+8*x+4
 
 
-    for i in list :
-        string = ""
-        for y in i:
-            string+=y
 
-        print(string)
+    print (list)
+"""
 
+def drawSquarre(size, y_center, x_center, image, color=[0, 255, 0]):
 
-drawCircle(2, 0, 0, "test", "test")"""
+    if(size > 0):
+        image[x_center][y_center] = color
+
+    for x in range(-size+1, size):
+        for y in range(-size+1, size):
+
+            image[x_center + x][y_center + y]=color
+
+def drawMap(min_x, max_x, min_y, max_y, image, color=[0, 255, 0]):
+
+    for x in range(min_x, max_x):
+
+        for y in range (min_y, max_y):
+
+            image[x][y] = color
+
 
 """ ***************************** for modifying pixel ******************************** """
 
